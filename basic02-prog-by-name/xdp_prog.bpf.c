c3dfc56ab96047d85b19c9bd33bbf77fc278c135
shichaoyuan <oahcihsnauy@gmail.com>
2025-05-14 23:20:31 +0800

    init

---
+++ basic02-prog-by-name/xdp_prog.bpf.c
@@ -0,0 +1,22 @@
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+
+SEC("xdp")
+int xdp_pass_func(struct xdp_md *ctx)
+{
+    return XDP_PASS;
+}
+
+SEC("xdp")
+int xdp_drop_func(struct xdp_md *ctx)
+{
+    return XDP_DROP;
+}
+
+SEC("xdp")
+int xdp_abort_func(struct xdp_md *ctx)
+{
+    return XDP_ABORTED;
+}
+
+char _license[] SEC("license") = "GPL";
